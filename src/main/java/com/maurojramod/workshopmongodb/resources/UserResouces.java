c66c93acd3c149b699d6f2a466d2b62ddc08bc6a
MauroJRamos <mauroramosti@gmail.com>
2023-09-25 18:01:23 -0300

    UserDTO

--- src/main/java/com/maurojramod/workshopmongodb/resources/UserResouces.java
+++ src/main/java/com/maurojramod/workshopmongodb/resources/UserResouces.java
@@ -1,8 +1,7 @@
 package com.maurojramod.workshopmongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maurojramod.workshopmongodb.domain.User;
+import com.maurojramod.workshopmongodb.dto.UserDTO;
 import com.maurojramod.workshopmongodb.services.UserService;
 
 
@@ -22,10 +22,10 @@ public class UserResouces {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
- 	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-	}
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	
-
+	}
 }
