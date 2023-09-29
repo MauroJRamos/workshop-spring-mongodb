c3977532743d16a28f04c92295e1d59f3f250a9f
MauroJRamos <mauroramosti@gmail.com>
2023-09-28 22:47:27 -0300

    POST

--- src/main/java/com/maurojramod/workshopmongodb/resources/UserResouces.java
+++ src/main/java/com/maurojramod/workshopmongodb/resources/UserResouces.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.maurojramod.workshopmongodb.domain.Post;
 import com.maurojramod.workshopmongodb.domain.User;
 import com.maurojramod.workshopmongodb.dto.UserDTO;
 import com.maurojramod.workshopmongodb.services.UserService;
@@ -59,5 +60,11 @@ public class UserResouces {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPost());
 	}
 }
