e095f69aa6cb234d9cf5d2488d0fc3e67116e3f3
MauroJRamos <mauroramosti@gmail.com>
2023-09-28 22:23:44 -0300

    add post to user

--- src/main/java/com/maurojramod/workshopmongodb/domain/User.java
+++ src/main/java/com/maurojramod/workshopmongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.maurojramod.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -14,7 +17,10 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
-	
+	@DBRef(lazy = true)
+	private List<Post> post = new ArrayList<>();
+ 	
+
 	public User() {
 	}
 
@@ -43,6 +49,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 		
+	}
+	
+	public List<Post> getPost() {
+		return post;
+	}
+
+	public void setPost(List<Post> post) {
+		this.post = post;
 	}
 	
 	@Override
